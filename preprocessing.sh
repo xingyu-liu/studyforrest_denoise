c6d8c29fafce549b3004ad22e1344a73c1cc3d2a
Xingyu Liu <liuxingyu987@gmail.com>
2019-06-01 15:02:06 +0800

    Update preprocessing.sh
    add annotation

--- preprocessing.sh
+++ preprocessing.sh
@@ -1,6 +1,13 @@
 # Preproccess functional MRI data with FSL feat
+# Example: sub-01, run-01
+# The preprocessing of the functional images was performed using FEAT 
+# (FMRIB Expert Analysis Tool version 6.00) including motion correction, 
+# slice timing correction, brain extraction and high-pass temporal 
+# filtering (200 s cutoff)22. Notably, we preporcessed the fMRI data 
+# with two spatical smoothing settings: unsmoothing and 
+# smoothing (Gaussian kernel; FWHM = 5 mm).
 
-# spatial smoothing with 5mm FWHM
+# spatial smoothing with 5mm FWHM 
 feat preprocessing_design_sm5.fsf
 
 # non spatial smoothing
